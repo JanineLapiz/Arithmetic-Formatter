032203feca20128a41e8861f0ec5b8838a762537
Janine Lapiz <janine@restoplus.com.au>
2023-10-22 00:41:46 +0800

    Fixed issues

--- main.py
+++ main.py
@@ -8,25 +8,25 @@ def format_arithmetic(problems: List[str], print_result: bool = False) -> str:
 
     formatted_problems = map(problem_formatter.format, problems)
 
-    line_1: List[str] = []
-    line_2: List[str] = []
-    line_3: List[str] = []
+    _line_1: List[str] = []
+    _line_2: List[str] = []
+    _line_3: List[str] = []
 
     for formatted_problem in formatted_problems:
-        [_line_1, _line_2, _line_3]= formatted_problem.splitlines()
+        [problem_line_1, problem_line_2, problem_line_3]= formatted_problem.splitlines()
 
-        line_1.append(_line_1)
-        line_2.append(_line_2)
-        line_3.append(_line_3)
+        _line_1.append(problem_line_1)
+        _line_2.append(problem_line_2)
+        _line_3.append(problem_line_3)
 
     gap = ' ' * 4
 
-    line_1: str = gap.join(line_1)
-    line_2: str = gap.join(line_2)
-    line_3: str = gap.join(line_3)
+    line_1: str = gap.join(_line_1)
+    line_2: str = gap.join(_line_2)
+    line_3: str = gap.join(_line_3)
 
     formatted_arithmetic = '\n'.join([line_1, line_2, line_3])
 
-    if (print_result): print(format_arithmetic)
+    if (print_result): print(formatted_arithmetic)
 
     return formatted_arithmetic
