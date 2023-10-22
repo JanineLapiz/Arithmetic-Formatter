d30bb39a768781f23c3c58382bbc746654f9f513
Janine Lapiz <janine@restoplus.com.au>
2023-10-22 19:20:38 +0800

    Minor edit

--- tests/test_arithmetic_formatter.py
+++ tests/test_arithmetic_formatter.py
@@ -3,7 +3,7 @@ from main import format_arithmetic
 
 class TestArithmeticFormatter(TestCase):
     def test_valid_problems(self):
-        result: str = format_arithmetic(['123 + 34', '56 - 1000', '180 + 180'], True)
+        result: str = format_arithmetic(['123 + 34', '56 - 1000', '180 + 180'])
 
         expected_result: str = (
             '  123        56      180\n' +
